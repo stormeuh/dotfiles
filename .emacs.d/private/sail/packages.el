14e781cb9ee4250b302f8d906aa61b8128edd90c
Elias Storme <elias.storme@gmail.com>
2021-11-12 12:49:17 +0100

    got sail mode working

--- .emacs.d/private/sail/packages.el
+++ .emacs.d/private/sail/packages.el
@@ -9,7 +9,9 @@
 	  )
 (defun sail/init-sail-mode () (use-package sail-mode
                                  :defer t
-                                 :init
-                                 (progn
-                                   (autoload 'sail2-mode "sail" "Major mode for editing sail programs." t))
-                                 ))
+                                :init
+                                (progn
+                                  (autoload 'sail2-mode "sail-mode" "Major mode for editing sail programs." t)))
+       )
+(add-to-list 'auto-mode-alist '("\\.sail\\'" . sail2-mode))
+;(defun sail/init-sail-mode () (require sail-mode))
